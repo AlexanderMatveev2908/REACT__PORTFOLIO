f2c6c7288336dc279058b2e4eea6c51589ad0110
Alex <alex@gmail.com>
2025-04-28 09:45:20 +0300

    made hero responsive, around 79% responsive

--- src/common/Hero/Hero.tsx
+++ src/common/Hero/Hero.tsx
@@ -5,20 +5,22 @@ import "./Hero.css";
 
 const Hero: FC = () => {
   return (
-    <section className="w-full grid py-[64px] h-fit items-start gap-[64px] px-[16px] sm:px-[128px]">
+    <section className="w-full grid xl:grid-cols-2 xl:gap-[310px] py-[64px] h-fit items-start gap-[64px] px-[16px] sm:px-[64px] xl:px-[128px]">
       {/* LEFT / UP */}
       <div className="w-full grid grid-cols-1 items-start h-fit">
-        <div className="w-full grid grid-cols-1 h-fit items-start justify-items-center">
+        <div className="w-full grid grid-cols-1 h-fit items-start justify-items-center xl:justify-items-start">
           <h6 className="txt__yell_0 txt_p_hero">Hey, I'm</h6>
           <h1 className="txt__yell_0 title_hero">ALEXANDER MATVEEV</h1>
-          <h1 className="title_hero txt__blue_0 ">Software Developer</h1>
-          <p className="txt__yell_0 txt_p_hero text-center">
+          <h1 className="title_hero txt__blue_0 xl:text-start ">
+            Software Developer
+          </h1>
+          <p className="txt__yell_0 txt_p_hero text-center xl:text-start">
             I am currently a student at SDA Academy where I study frontend
             development, while I am exploring backend on my own and creating
             full-stack projects.
           </p>
         </div>
-        <div className="mt-[16px] w-full flex items-center gap-[16px] justify-center">
+        <div className="mt-[16px] w-full flex items-center gap-[16px] justify-center xl:justify-start">
           <a
             href="https://github.com/AlexanderMatveev2908"
             className="w-[32px] h-[32px]"
@@ -26,24 +28,23 @@ const Hero: FC = () => {
             <img src={icons.git} alt="" className="w-full h-full" />
           </a>
         </div>
-        <div className="mt-[24px] w-full flex justify-center">
+        <div className="mt-[24px] w-full flex justify-center xl:justify-start">
           <Btn {...{ label: "Let's work together" }} />
         </div>
       </div>
       {/* RIGHT / DOWN */}
-      <div className="w-full flex justify-center items-center">
-        <div className="flex justify-center max-w-[396px] h-[322px] relative items-center">
-          <div className="h-[64px] w-[80px] bg__blue_1 absolute z-10 rounded-xl bottom-0 translate-y-1/2 left-0 -translate-x-[40%]"></div>
-
-          <div className="w-[314px] h-[324px]">
+      <div className="w-full flex justify-center items-center xl:h-full">
+        <div className="flex justify-center w-full h-fit items-center">
+          <div className="w-[80%] h-fit relative xl:w-full">
+            <div className="h-[64px] w-[80px] bg__blue_1 absolute z-10 rounded-xl bottom-0 translate-y-1/2 left-0 -translate-x-[40%]"></div>
             <img
               src={imgs.hero_0}
               alt=""
-              className="w-full h-full object-cover relative z-20"
+              className="w-full object-cover relative z-20"
             />
-          </div>
 
-          <div className="h-[82px] w-[106px] bg__blue_1 absolute z-10 rounded-xl top-0 right-[2%] -translate-y-[30%] translate-x-[40%]"></div>
+            <div className="h-[82px] w-[106px] bg__blue_1 absolute z-10 rounded-xl top-0 right-[2%] -translate-y-[30%] translate-x-[40%]"></div>
+          </div>
         </div>
       </div>
     </section>
